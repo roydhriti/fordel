07d2ea73dbfc4c3f2b6bcd89ee87c22fc8d1eb4f
Rakesh Das <rakesh.das@digitalwoods.com>
2024-11-22 09:48:58 +0530

    fix

--- src/pages/HomePage/components/OurServicesSection/OurServicesSection.tsx
+++ src/pages/HomePage/components/OurServicesSection/OurServicesSection.tsx
@@ -54,10 +54,59 @@ const OurServicesSection: FC<OurServicesSectionProps> = () => (
           </div>
         </div>
       </div>
+      <div className={styles.ServicesCard}>
+        <div className={styles.ImageWrap}>
+          <img
+            src="https://plus.unsplash.com/premium_photo-1663045286919-b66b756d93be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZGV2ZWxvcG1lbnQlMjBzb2Z0d2FyZXxlbnwwfHwwfHx8MA%3D%3D"
+            alt="UX/UI Design Services Image"
+          />
+        </div>
 
-      <div className={styles.ServicesCard}></div>
+        <div className={styles.ServiceContent}>
+          <div className={styles.ContentHeader}>
+            <h5 className="sv1">Development Services</h5>
+          </div>
 
-      <div className={styles.ServicesCard}></div>
+          <div className={styles.ContentBody}>
+            <p className="bodyV2"></p>
+          </div>
+
+          <div className={styles.ContentTags}>
+            <p className="overline">Innovative</p>
+
+            <div className={styles.Circle}></div>
+
+            <p className="overline">Iterative</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.ServicesCard}>
+        <div className={styles.ImageWrap}>
+          <img
+            src="https://images.unsplash.com/photo-1580983561371-7f4b242d8ec0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8UXVhbGl0eSUyMGNvbnRyb2wlMjBzb2Z0d2FyZXxlbnwwfHwwfHx8MA%3D%3D"
+            alt="UX/UI Design Services Image"
+          />
+        </div>
+
+        <div className={styles.ServiceContent}>
+          <div className={styles.ContentHeader}>
+            <h5 className="sv1">Development Services</h5>
+          </div>
+
+          <div className={styles.ContentBody}>
+            <p className="bodyV2"></p>
+          </div>
+
+          <div className={styles.ContentTags}>
+            <p className="overline">Innovative</p>
+
+            <div className={styles.Circle}></div>
+
+            <p className="overline">Iterative</p>
+          </div>
+        </div>
+      </div>
 
       <div className={styles.ServicesCard}></div>
 
